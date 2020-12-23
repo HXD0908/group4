8b2df6b6473f2f51fd1f72fa478f1c90bef5a982
HXD <2486985510@qq.com>
2020-12-24 07:51:50 +0800

    韩虎的作业

---
+++ day08/韩虎的作业/韩虎/app/src/main/java/com/example/map/SearchItemAdapter.java
@@ -0,0 +1,28 @@
+package com.example.map;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.baidu.mapapi.search.core.PoiInfo;
+
+import java.util.List;
+
+public class SearchItemAdapter extends BaseAdapter<PoiInfo> {
+
+    public SearchItemAdapter(Context context, List<PoiInfo> list){
+        super(list, context);
+    }
+
+    @Override
+    protected void bindData(PoiInfo poiInfo, VH vh, int position, Context context) {
+        TextView txtName = (TextView) vh.getViewById(R.id.txt_name);
+        txtName.setText(poiInfo.name+" "+poiInfo.address);
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.layout_search_item;
+    }
+
+
+}
